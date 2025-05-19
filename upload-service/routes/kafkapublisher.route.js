68bcb4936dfb6a107ba5cd1d721a12870d18856b
Harsh Singh <harshsingh@Harshs-MacBook-Air.local>
2025-05-20 00:26:08 +0530

    Moved root project into client folder

--- upload-service/routes/kafkapublisher.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import sendMessageToKafka from "../controllers/kafkapublisher.controller.js";
-
-const router = express.Router();
-router.post('/', sendMessageToKafka);
-
-export default router;
